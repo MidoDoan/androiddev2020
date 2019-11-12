8d9fbd0845c07edebaaaba9d055ba80d2d13ca5a
MidoDoan <huongdl.hx@gmail.com>
2019-11-12 22:51:08 +0700

    Practical Work 3

---
+++ USTHWeathernew/app/src/main/java/vn/edu/usth/usthweathernew/ForecastFragment.java
@@ -0,0 +1,33 @@
+package vn.edu.usth.usthweathernew;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ForecastFragment extends Fragment {
+
+
+    public ForecastFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
+        view.setBackgroundColor(0xff2299ee);
+        return view;
+    }
+
+}
--- USTHWeathernew/app/src/main/java/vn/edu/usth/usthweathernew/WeatherActivity.java
+++ USTHWeathernew/app/src/main/java/vn/edu/usth/usthweathernew/WeatherActivity.java
@@ -2,46 +2,53 @@ package vn.edu.usth.usthweathernew;
 
 import android.os.Bundle;
 import android.util.Log;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
-public class WeatherActivity extends AppCompatActivity {
+public class WeatherActivity extends FragmentActivity {
 
-    private static final String TAG ="WeatherActivity";
+    private static final String TAG = "WeatherActivity";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i(TAG, "This is onCreate" );
+        setContentView(R.layout.weather_activity);
+        Log.i(TAG, "This is onCreate");
+        // Create a new Fragment to be placed in the weather activity
+        ForecastFragment firstFragment = new ForecastFragment();
+        // Add the fragment to the 'container' FrameLayout
+        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i(TAG, "This is onPause" );
+        Log.i(TAG, "This is onPause");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i(TAG, "This is onDestroy" );
+        Log.i(TAG, "This is onDestroy");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(TAG, "This is onStart" );
+        Log.i(TAG, "This is onStart");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i(TAG, "This is onStop" );
+        Log.i(TAG, "This is onStop");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(TAG, "This is onResume" );
+        Log.i(TAG, "This is onResume");
     }
 }
