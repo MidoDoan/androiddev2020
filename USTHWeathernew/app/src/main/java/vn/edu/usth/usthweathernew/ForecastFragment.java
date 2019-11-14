29fec9ad2be723fa34f6c8af214e7ebb25fdf5b1
MidoDoan <huongdl.hx@gmail.com>
2019-11-14 15:59:50 +0700

    Practical Work 4

--- USTHWeathernew/app/src/main/java/vn/edu/usth/usthweathernew/ForecastFragment.java
+++ USTHWeathernew/app/src/main/java/vn/edu/usth/usthweathernew/ForecastFragment.java
@@ -1,13 +1,19 @@
 package vn.edu.usth.usthweathernew;
 
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 /**
@@ -26,8 +32,42 @@ public class ForecastFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_forecast, container, false);
-        view.setBackgroundColor(0xff2299ee);
-        return view;
+        //view.setBackgroundColor(0xff2299ee);
+        LinearLayout linearLayout = new LinearLayout(getActivity());
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setPadding(10, 10, 10, 10);
+        linearLayout.setGravity(Gravity.CENTER);
+        linearLayout.setBackgroundResource(R.drawable.gradient);
+        linearLayout.addView(addTextView());
+        linearLayout.addView(addImageView());
+
+        return linearLayout;
     }
 
+    private TextView addTextView() {
+        TextView textView = new TextView(getActivity());
+        textView.setText("Thursday");
+        textView.setTextColor(Color.WHITE);
+        textView.setTextSize(25);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTypeface(Typeface.DEFAULT_BOLD);
+        LinearLayout.LayoutParams textViewParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        textView.setLayoutParams(textViewParams);
+        return textView;
+    }
+
+    private ImageView addImageView() {
+        ImageView imageView = new ImageView(getActivity());
+        imageView.setImageResource(R.drawable.cloudy);
+        imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        LinearLayout.LayoutParams imageViewParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        imageView.setLayoutParams(imageViewParams);
+        return imageView;
+    }
 }
