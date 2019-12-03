c2c548f2c41722d3f8835bdb905f10c1138321d6
MidoDoan <huongdl.hx@gmail.com>
2019-12-04 06:38:53 +0700

    Practical Work 11

--- USTHWeathernew/app/src/main/java/vn/edu/usth/usthweathernew/WeatherActivity.java
+++ USTHWeathernew/app/src/main/java/vn/edu/usth/usthweathernew/WeatherActivity.java
@@ -1,6 +1,9 @@
 package vn.edu.usth.usthweathernew;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
@@ -13,9 +16,19 @@ import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class WeatherActivity extends AppCompatActivity {
 
     private static final String TAG = "WeatherActivity";
+    private File file;
+    private String filename = "mikrokomos.mp3";
+    private String filepath;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,13 +46,45 @@ public class WeatherActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
         tabLayout.setupWithViewPager(pager);
 
+        
+        filepath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Android/data/vn.edu.usth.usthweathernew/files/";
+        writeExternal();
+        MediaPlayer mediaPlayer= new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource(filepath+filename);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mediaPlayer.start();
+    }
 
+    private void writeExternal() {
+        try {
+            InputStream is = getApplicationContext().getResources().openRawResource(R.raw.mikrokomos);
+            file = new File(filepath + filename);
+            file.createNewFile();
+            OutputStream fout = new FileOutputStream(file);
+            byte[] buffer = new byte[1024];
+            while (true) {
+                int bytesRead = is.read(buffer);
+                if (bytesRead == -1)
+                    break;
+                fout.write(buffer, 0, bytesRead);
+            }
+            fout.close();
+            is.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Log.i(TAG, "This is onPause");
+
     }
 
     @Override
